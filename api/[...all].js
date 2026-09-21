303914dae359a58b79282a58a214a5d8c25d93cb
Thara2008 <91526145+Thara2008@users.noreply.github.com>
2026-09-21 13:30:02 +0530

    Update [...all].js

--- api/[...all].js
+++ api/[...all].js
@@ -22,8 +22,8 @@ const { redisStorage } = require('../lib/storage');
 const { handleApi, bootstrapAdmin } = require('../lib/handler');
 
 module.exports = async (req, res) => {
-  const url = process.env.UPSTASH_REDIS_REST_URL;
-  const token = process.env.UPSTASH_REDIS_REST_TOKEN;
+  const url = process.env.UPSTASH_REDIS_REST_URL || process.env.KV_REST_API_URL;
+const token = process.env.UPSTASH_REDIS_REST_TOKEN || process.env.KV_REST_API_TOKEN;
   if (!url || !token){
     res.statusCode = 500;
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
